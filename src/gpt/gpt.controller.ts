659f5f86386f158548f9d4058df4775c8bfac034
Max Alva <alva85@gmail.com>
2024-02-27 06:42:21 -0500

    Validaciones de la peticion

--- src/gpt/gpt.controller.ts
+++ src/gpt/gpt.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { GptService } from './gpt.service';
+import { OrthographyDto } from './dtos';
 
 @Controller( 'gpt' )
 export class GptController {
@@ -7,7 +8,7 @@ export class GptController {
   }
 
   @Post( 'orthography-check' )
-  orthographyCheck() {
+  orthographyCheck( @Body() orthographyDto: OrthographyDto ) {
     return this.gptService.orthographyCheck();
   }
 }
