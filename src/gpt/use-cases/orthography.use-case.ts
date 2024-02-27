d4ae4d230394ebc8ccd96975f7f07ed596e1f2db
Max Alva <alva85@gmail.com>
2024-02-27 07:25:22 -0500

    Nuestro primer prompt

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -1,9 +1,24 @@
+import OpenAI from 'openai';
+
 interface Options {
   prompt: string;
 }
 
-export const orthographyUseCase = async ( options: Options ) => {
+export const orthographyUseCase = async ( openai: OpenAI, options: Options ) => {
   const { prompt } = options;
+
+  const completion = await openai.chat.completions.create( {
+    messages: [
+      { role: 'system', content: 'Tu nombre es Jarvis, siempre debes responder amablemente' },
+      {
+        role: 'user',
+        content: prompt
+      }
+    ],
+    model: 'gpt-3.5-turbo'
+  } );
+
+  return completion.choices[ 0 ];
 
   return {
     prompt
