c8a7134f906f743c964f6538a9e23639ae8f62f5
Max Alva <alva85@gmail.com>
2024-02-27 08:00:12 -0500

    Corrector de ortografía - Use Case

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -9,13 +9,32 @@ export const orthographyUseCase = async ( openai: OpenAI, options: Options ) => 
 
   const completion = await openai.chat.completions.create( {
     messages: [
-      { role: 'system', content: 'Tu nombre es Jarvis, siempre debes responder amablemente' },
+      {
+        role: 'system',
+        content: `
+        Te serán proveídos textos en español con posibles errores ortográficos y gramaticales,
+        las palabras usadas deben de existir en el diccionario de la Real Academia Española,
+        debes de responder en formato JSON,
+        tu tarea es corregirlos y retornar información de las solicitudes,
+        también debes de dar un porcentaje de acierto por el usuario.
+        
+        Si no hay errores, debes de retornar un mensaje indicando que no hay errores.
+        
+        {
+          userScore: number,
+          errors: string[]. // ['error -> solución'],
+          message: string, // usa emojis y texto para dar retroalimentación al usuario
+        }
+        `
+      },
       {
         role: 'user',
         content: prompt
       }
     ],
-    model: 'gpt-3.5-turbo'
+    model: 'gpt-3.5-turbo',
+    temperature: 0.3,
+    max_tokens: 150
   } );
 
   return completion.choices[ 0 ];
